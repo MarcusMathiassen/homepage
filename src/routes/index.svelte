8cc6c6676de58baeea75d6fc6d8dfd29fc31fbca
Marcus Mathiassen <mathiassenmarcus@me.com>
2019-09-06 22:33:48 +0200

    '../..'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,47 +1,47 @@
 <script>
     import P2D from '../components/p2d.svelte'
 
-       fetch(`https://api.github.com/graphql`, {
-        method: 'post',
-        headers: {
-            Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: `query { viewer { repositories(first: 20) { edges { node { name pushedAt } } } } }`,
-        }),
-    })
-        .then(res => res.json())
-        .then(json => {
-            var edges = json.data.viewer.repositories.edges
-            // Sort the repositories by pushdate
-            edges.sort((a, b) => {
-                return (
-                    new Date(b.node.pushedAt).getTime() -
-                    new Date(a.node.pushedAt).getTime()
-                )
-            })
-            const nameOfLastRepoUpdated = edges[0].node.name
-            fetch(`https://api.github.com/graphql`, {
-                method: 'post',
-                headers: {
-                    Authorization:
-                        `bearer ${process.env.GITHUB_TOKEN}`,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    query: `query { viewer { repository(name:"${nameOfLastRepoUpdated}") { description } } }`,
-                }),
-            })
-                .then(res => res.json())
-                .then(json => {
-                    console.log(json)
-                    const desc = json.data.viewer.repository.description
-                    // $(
-                    //     '#last-repo-updated'
-                    // ).innerHTML = `Currently working on <h1>${nameOfLastRepoUpdated}</h1> <p>${desc}</p>`
-                })
-        })
+    //    fetch(`https://api.github.com/graphql`, {
+    //     method: 'post',
+    //     headers: {
+    //         Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         query: `query { viewer { repositories(first: 20) { edges { node { name pushedAt } } } } }`,
+    //     }),
+    // })
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         var edges = json.data.viewer.repositories.edges
+    //         // Sort the repositories by pushdate
+    //         edges.sort((a, b) => {
+    //             return (
+    //                 new Date(b.node.pushedAt).getTime() -
+    //                 new Date(a.node.pushedAt).getTime()
+    //             )
+    //         })
+    //         const nameOfLastRepoUpdated = edges[0].node.name
+    //         fetch(`https://api.github.com/graphql`, {
+    //             method: 'post',
+    //             headers: {
+    //                 Authorization:
+    //                     `bearer ${process.env.GITHUB_TOKEN}`,
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({
+    //                 query: `query { viewer { repository(name:"${nameOfLastRepoUpdated}") { description } } }`,
+    //             }),
+    //         })
+    //             .then(res => res.json())
+    //             .then(json => {
+    //                 console.log(json)
+    //                 const desc = json.data.viewer.repository.description
+    //                 // $(
+    //                 //     '#last-repo-updated'
+    //                 // ).innerHTML = `Currently working on <h1>${nameOfLastRepoUpdated}</h1> <p>${desc}</p>`
+    //             })
+    //     })
 
 </script>
 
