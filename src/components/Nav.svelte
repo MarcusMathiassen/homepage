3df84ef9973d3579a0fc479df120cba495a5e6c0
Marcus Mathiassen <mathiassenmarcus@me.com>
2019-09-07 14:55:53 +0200

    'removed projects page'

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -63,11 +63,6 @@
                 about
             </a>
         </li>
-        <li>
-            <a class={segment === 'projects' ? 'selected' : ''} href="projects">
-                projects
-            </a>
-        </li>
         <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
         <li>
