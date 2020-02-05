b54d0037cc7f4791292e3a0cd95b54430d9015c1
Marcus Mathiassen <marcusmathiassen@Marcuss-MacBook-Pro.local>
2020-02-05 21:20:08 +0100

    '../..'

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-    content: ["**/*.html"],
-    whitelist: []
+  content: ["**/*.html"],
+  whitelist: ["badge", "box", "section", "container",
+    "icon-star",
+    "icon-heart",
+    "icon-eye",
+    "icon-comment",
+    "icon-github-circled",
+    "icon-mail-alt",
+    "icon-fork",
+    "icon-youtube-play",
+  ]
 }
